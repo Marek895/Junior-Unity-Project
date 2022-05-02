f489d9a988e64a6fa9d86c3b3ba3b4398fbc4ec0
Marek895 <marek.neubauer89@wp.pl>
2022-05-02 17:19:55 +0200

    Refactored AgentHealth script

--- Assets/Scripts/AgentHealth.cs
+++ Assets/Scripts/AgentHealth.cs
@@ -20,37 +20,45 @@ public class AgentHealth : MonoBehaviour
         currentHealthpoints = maxHealthPoints;
     }
 
-    void OnTriggerEnter(Collider other) 
+    void OnTriggerEnter(Collider other)
     {
+        ProcessCollision();
+        ChangeAgentCollor(); // change agent colllor when collides with another
+    }
 
+    private void ChangeAgentCollor()
+    {
+        foreach (Transform bodypart in transform) 
+        {
+
+            if (currentHealthpoints == 2)
+            {
+                bodypart.GetComponent<MeshRenderer>().material = agenttInjuredMaterial;
+            }
+            else if (currentHealthpoints == 1)
+            {
+                bodypart.GetComponent<MeshRenderer>().material = agenttBadlyInjuredMaterial;
+            }
+
+        }
+    }
+
+    private void ProcessCollision()
+    {
         currentHealthpoints--;
 
-        if(transform.childCount>4) //Selected agent can have updated HP on UI when touches another agent
+        if (transform.childCount > 4) //Selected agent can have updated HP on UI when touches another agent
         {
             updateAgentHP?.Invoke(gameObject.GetComponent<AgentHealth>().currentHealthpoints, gameObject.name);
         }
-        if(currentHealthpoints <=0)
+        if (currentHealthpoints <= 0)
         {
-            if(transform.childCount>4) //Agent Selection Mark is always 5th child of agent Object , so when is dies with Agent Selection Mark on it, the UI is cleared
+            if (transform.childCount > 4) //Agent Selection Mark is always 5th child of agent Object , so when is dies with Agent Selection Mark on it, the UI is cleared
             {
                 clearAgentSelection?.Invoke();
             }
 
             AgentDespawn();
-        }
-
-        foreach(Transform bodypart in transform) // change agent colllor when collides with another
-        {
-
-            if(currentHealthpoints == 2)
-            {
-                bodypart.GetComponent<MeshRenderer>().material= agenttInjuredMaterial;
-            }
-            else if (currentHealthpoints ==1)
-            {
-                bodypart.GetComponent<MeshRenderer>().material= agenttBadlyInjuredMaterial;
-            }
-            
         }
     }
 
