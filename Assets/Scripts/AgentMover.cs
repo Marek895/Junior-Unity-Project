999d9409b90e6c5ec29d2f8a321747c2932a4b0b
Marek895 <marek.neubauer89@wp.pl>
2022-05-02 17:14:05 +0200

    Refactored AgentMover script

--- Assets/Scripts/AgentMover.cs
+++ Assets/Scripts/AgentMover.cs
@@ -1,41 +1,55 @@
-
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AgentMover : MonoBehaviour
 {
+    [SerializeField] float agentSpeed = 2f;
+    [SerializeField] float movementDuration = 5f;
     Vector3[] vectors = new Vector3[] {Vector3.forward, Vector3.back, Vector3.left, Vector3.right};
     Vector3 startposition;
     int vectorIndex;
-    bool vectorChoosed2 = false;
-    bool vectorChoosed4 = false;
-    bool vectorChoosed6 = false;
     float distance ;
+    float currentTime;
+    
 
     void Start()
     {
         startposition = transform.position; //setting initial position so we can check the distance
-        ChooseVector();
-
+        currentTime = Time.time;
+        ChooseVector(); // choosa random starting vector 
     }
-
 
     void Update()
     {
-        distance = Vector3.Distance(startposition, transform.position);
+        CheckDistance();
+        ControlAgent();
+    }
+
+    void ChooseVector() 
+    {
+        int currentVectorIndex = vectorIndex;
+        while (currentVectorIndex == vectorIndex)
+        {
+            vectorIndex = Random.Range(0, 4);
+        }
         
+    }
+
+    private void CheckDistance()
+    {
+        distance = Vector3.Distance(startposition, transform.position);
+    }
+
+
+    void ControlAgent()
+    {
+        AvoidBondary(); //switch Vector if Agent approach a bondary
+        ChangeDirection(); // agent is able to change direction after some time
         MoveAgent();
-        ResetVector();
     }
 
-    void ChooseVector() // choosa random starting vector 
+    private void AvoidBondary() 
     {
-        vectorIndex = Random.Range(0, 4);
-    }
-
-    void MoveAgent() //switch Vector if Agent approach a bondary
-    {
-
         if (transform.position.x >= 10)
         {
             vectorIndex = 2;
@@ -52,58 +66,19 @@ public class AgentMover : MonoBehaviour
         {
             vectorIndex = 0;
         }
-
-        ChangeDirection();
-
-        transform.Translate(vectors[vectorIndex] * Time.deltaTime);
     }
 
-    void ChangeDirection() // agent is able to to change direction after sme distance
+    void ChangeDirection() 
     {
-        if (distance > 2)
+        if (Time.time > currentTime + movementDuration)
         {
-            if (!vectorChoosed2)
-            {
-                ChooseVector(); //choose random moving vector while on the move
-                vectorChoosed2 = true;
-            }
-        }
-        if (distance > 4)
-        {
-            if (!vectorChoosed4)
-            {
-                ChooseVector();
-                vectorChoosed4 = true;
-            }
-        }
-
-        if (distance > 6)
-        {
-            if (!vectorChoosed6)
-            {
-                ChooseVector();
-                vectorChoosed6 = true;
-            }
+            ChooseVector();
+            currentTime = Time.time;
         }
     }
 
-
-    void ResetVector() // reset bools so agent can switch direction multiple times;
+    private void MoveAgent()
     {
-        if (distance<=6 && distance >=4)
-        {
-            vectorChoosed6 = false;
-        }
-
-        if (distance<=4 && distance >=2)
-        {
-            vectorChoosed4 = false;
-        }
-
-        if (distance<=2)
-        {
-            vectorChoosed4 = false;
-        }
+        transform.Translate(vectors[vectorIndex] * Time.deltaTime * agentSpeed);
     }
-
 }
