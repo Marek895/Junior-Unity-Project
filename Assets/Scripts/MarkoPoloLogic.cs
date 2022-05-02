e0b6feda4faf5e4310e0869d319405c3e0dda3f5
Marek895 <marek.neubauer89@wp.pl>
2022-05-02 17:24:23 +0200

    Refactored MarkoPolo

--- Assets/Scripts/MarkoPoloLogic.cs
+++ Assets/Scripts/MarkoPoloLogic.cs
@@ -7,26 +7,31 @@ public class MarkoPoloLogic : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI markoPoloText;
 
-    private void Start() 
+    private void Start()
     {
-        for (int i = 1; i<101; i++)
+        MarkoPolo();
+    }
+
+    private void MarkoPolo()
+    {
+        for (int i = 1; i < 101; i++)
         {
-            if (i % 3 == 0 && i % 5 ==0)
-            markoPoloText.text +=   "<br> MarkoPolo";
+            if (i % 3 == 0 && i % 5 == 0)
+                markoPoloText.text += "<br> MarkoPolo";
 
             else if (i % 3 == 0)
             {
-                markoPoloText.text +=  "<br> Marko";
+                markoPoloText.text += "<br> Marko";
             }
 
             else if (i % 5 == 0)
             {
-                markoPoloText.text +=  "<br> Polo";
+                markoPoloText.text += "<br> Polo";
             }
 
-            else 
+            else
             {
-                markoPoloText.text +=  "<br>" +  i.ToString();
+                markoPoloText.text += "<br>" + i.ToString();
             }
 
         }
